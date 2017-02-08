0563df929e612e1bbc88ee4f71a74f603c07463b
YSRKEN <ysr.ken@gmail.com>
2017-02-09 00:11:56 +0900

    制約式の数・名前・範囲を定めるところまで

---
+++ GlpkWrapperCS/GlpkWrapperCS.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlpkWrapperCS {
+	using org.gnu.glpk;
+	enum ObjectDirection { Minimize = 1, Maximize = 2 }
+	enum BoundsType { Free, Lower, Upper, Double, Fixed }
+	class MipProblem : IDisposable {
+		glp_prob problem = GLPK.glp_create_prob();
+		// コンストラクタ
+		public MipProblem() {
+			RowName = new rowName(this);
+			RowType = new rowType(this);
+			RowLB = new rowLb(this);
+			RowUB = new rowUb(this);
+		}
+		// Disposeメソッド
+		public void Dispose() { }
+		// 問題名
+		public string Name {
+			get { return GLPK.glp_get_prob_name(problem); }
+			set { GLPK.glp_set_prob_name(problem, value); }
+		}
+		// 最適化の方向
+		public ObjectDirection ObjDir {
+			get { return (ObjectDirection)GLPK.glp_get_obj_dir(problem); }
+			set { GLPK.glp_set_obj_dir(problem, (int)value); }
+		}
+		// 制約式を追加する
+		public void AddRows(int n) {
+			GLPK.glp_add_rows(problem, n);
+		}
+		// 制約式の数
+		public int RowsCount {
+			get { return GLPK.glp_get_num_rows(problem); }
+		}
+		// 制約式の名前
+		public class rowName {
+			MipProblem mip;
+			public rowName(MipProblem mip) {
+				this.mip = mip;
+			}
+			public string this[int index] {
+				get { return GLPK.glp_get_row_name(mip.problem, index + 1); }
+				set { GLPK.glp_set_row_name(mip.problem, index + 1, value); }
+			}
+		}
+		public rowName RowName;
+		// 制約式の範囲を設定する
+		public void SetRowBounds(int index, BoundsType type, double lowerBound, double upperBound) {
+			GLPK.glp_set_row_bnds(problem, index + 1, (int)type, lowerBound, upperBound);
+		}
+		// 制約式の種類
+		public class rowType {
+			MipProblem mip;
+			public rowType(MipProblem mip) {
+				this.mip = mip;
+			}
+			public BoundsType this[int index] {
+				get { return (BoundsType)GLPK.glp_get_row_type(mip.problem, index + 1); }
+			}
+		}
+		public rowType RowType;
+		// 制約式の下限
+		public class rowLb {
+			MipProblem mip;
+			public rowLb(MipProblem mip) {
+				this.mip = mip;
+			}
+			public double this[int index] {
+				get { return GLPK.glp_get_row_lb(mip.problem, index + 1); }
+			}
+		}
+		public rowLb RowLB;
+		// 制約式の上限
+		public class rowUb {
+			MipProblem mip;
+			public rowUb(MipProblem mip) {
+				this.mip = mip;
+			}
+			public double this[int index] {
+				get { return GLPK.glp_get_row_ub(mip.problem, index + 1); }
+			}
+		}
+		public rowUb RowUB;
+
+	}
+}
--- GlpkWrapperCS/Program.cs
+++ GlpkWrapperCS/Program.cs
@@ -3,10 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GlpkWrapperCS;
 
-namespace GlpkWrapperCS {
+namespace GlpkSample {
+	using org.gnu.glpk;
 	class Program {
 		static void Main(string[] args) {
+			Func1();
+			Console.WriteLine("");
+			Func2();
+		}
+		// GLPK for C#/CLIをそのまま使った場合
+		static void Func1() {
+			Console.WriteLine("【GLPK for C#/CLI】");
+			using(var problem = GLPK.glp_create_prob()) {
+				// 問題名
+				GLPK.glp_set_prob_name(problem, "sample");
+				// 最適化の方向
+				GLPK.glp_set_obj_dir(problem, GLPK.GLP_MAX);
+				// 制約式の数・名前・範囲
+				GLPK.glp_add_rows(problem, 3);
+				GLPK.glp_set_row_name(problem, 1, "p");
+				GLPK.glp_set_row_bnds(problem, 1, GLPK.GLP_UP, 0.0, 100.0);
+				GLPK.glp_set_row_name(problem, 2, "q");
+				GLPK.glp_set_row_bnds(problem, 2, GLPK.GLP_UP, 0.0, 600.0);
+				GLPK.glp_set_row_name(problem, 3, "r");
+				GLPK.glp_set_row_bnds(problem, 3, GLPK.GLP_UP, 0.0, 300.0);
+				// 変数の数・名前・範囲
+				GLPK.glp_add_cols(problem, 3);
+				GLPK.glp_set_col_name(problem, 1, "x1");
+				GLPK.glp_set_col_bnds(problem, 1, GLPK.GLP_LO, 0.0, 0.0);
+				GLPK.glp_set_col_name(problem, 2, "x2");
+				GLPK.glp_set_col_bnds(problem, 2, GLPK.GLP_LO, 0.0, 0.0);
+				GLPK.glp_set_col_name(problem, 3, "x3");
+				GLPK.glp_set_col_bnds(problem, 3, GLPK.GLP_LO, 0.0, 0.0);
+				// 目的関数の係数
+				GLPK.glp_set_obj_coef(problem, 1, 10.0);
+				GLPK.glp_set_obj_coef(problem, 2, 6.0);
+				GLPK.glp_set_obj_coef(problem, 3, 4.0);
+				// 制約式の係数
+
+			}
+		}
+		// GlpkWrapperCSを使った場合
+		static void Func2() {
+			Console.WriteLine("【GlpkWrapperCS】");
+			using(var problem = new MipProblem()) {
+				// 問題名
+				problem.Name = "sample";
+				// 最適化の方向
+				problem.ObjDir = ObjectDirection.Maximize;
+				// 制約式の数・名前・範囲
+				problem.AddRows(3);
+				problem.RowName[0] = "p";
+				problem.RowName[1] = "q";
+				problem.RowName[2] = "r";
+				problem.SetRowBounds(0, BoundsType.Upper, 0.0, 100.0);
+				problem.SetRowBounds(1, BoundsType.Upper, 0.0, 600.0);
+				problem.SetRowBounds(2, BoundsType.Upper, 0.0, 300.0);
+				/*// 変数の数・名前・範囲
+				GLPK.glp_add_cols(problem, 3);
+				GLPK.glp_set_col_name(problem, 1, "x1");
+				GLPK.glp_set_col_bnds(problem, 1, GLPK.GLP_LO, 0.0, 0.0);
+				GLPK.glp_set_col_name(problem, 2, "x2");
+				GLPK.glp_set_col_bnds(problem, 2, GLPK.GLP_LO, 0.0, 0.0);
+				GLPK.glp_set_col_name(problem, 3, "x3");
+				GLPK.glp_set_col_bnds(problem, 3, GLPK.GLP_LO, 0.0, 0.0);
+				// 目的関数の係数
+				GLPK.glp_set_obj_coef(problem, 1, 10.0);
+				GLPK.glp_set_obj_coef(problem, 2, 6.0);
+				GLPK.glp_set_obj_coef(problem, 3, 4.0);
+				// 制約式の係数*/
+
+			}
 		}
 	}
 }
