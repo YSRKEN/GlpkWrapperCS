e0717f59bb9230cf1edb5b096c6e54e0e6d01fc3
YSRKEN <ysr.ken@gmail.com>
2017-02-09 14:10:13 +0900

    とりあえず整数計画法を解くところまで実装

--- GlpkWrapperCS/GlpkWrapperCS.cs
+++ GlpkWrapperCS/GlpkWrapperCS.cs
@@ -7,15 +7,25 @@ using System.Threading.Tasks;
 namespace GlpkWrapperCS {
 	using org.gnu.glpk;
 	enum ObjectDirection { Minimize = 1, Maximize = 2 }
-	enum BoundsType { Free, Lower, Upper, Double, Fixed }
+	enum BoundsType { Free = 1, Lower = 2, Upper = 3, Double = 4, Fixed = 5 }
+	enum SolverResult {  }
 	class MipProblem : IDisposable {
 		glp_prob problem = GLPK.glp_create_prob();
 		// コンストラクタ
 		public MipProblem() {
+			// 目的関数関係
+			ObjCoef = new objCoef(this);
+			// 制約式関係
 			RowName = new rowName(this);
 			RowType = new rowType(this);
 			RowLB = new rowLb(this);
 			RowUB = new rowUb(this);
+			// 変数関係
+			ColumnName = new columnName(this);
+			ColumnType = new columnType(this);
+			ColumnLB = new columnLb(this);
+			ColumnUB = new columnUb(this);
+			LpColumnValue = new lpColumnValue(this);
 		}
 		// Disposeメソッド
 		public void Dispose() { }
@@ -24,11 +34,34 @@ namespace GlpkWrapperCS {
 			get { return GLPK.glp_get_prob_name(problem); }
 			set { GLPK.glp_set_prob_name(problem, value); }
 		}
+		// 最適化処理
+		public void Simplex() {
+			GLPK.glp_simplex(problem, null);
+		}
+		#region 目的関数関係
 		// 最適化の方向
 		public ObjectDirection ObjDir {
 			get { return (ObjectDirection)GLPK.glp_get_obj_dir(problem); }
 			set { GLPK.glp_set_obj_dir(problem, (int)value); }
 		}
+		// 目的関数の係数
+		public class objCoef {
+			MipProblem mip;
+			public objCoef(MipProblem mip) {
+				this.mip = mip;
+			}
+			public double this[int index] {
+				get { return GLPK.glp_get_obj_coef(mip.problem, index + 1); }
+				set { GLPK.glp_set_obj_coef(mip.problem, index + 1, (int)value); }
+			}
+		}
+		public objCoef ObjCoef;
+		// 最適化後の値
+		public double LpObjVal {
+			get { return GLPK.glp_get_obj_val(problem); }
+		}
+		#endregion
+		#region 制約式関係
 		// 制約式を追加する
 		public void AddRows(int n) {
 			GLPK.glp_add_rows(problem, n);
@@ -86,6 +119,94 @@ namespace GlpkWrapperCS {
 			}
 		}
 		public rowUb RowUB;
-
+		// 制約式の係数
+		public void LoadMatrix(int size, int[] ia, int[] ja, double[] ar) {
+			var ia_ = GLPK.new_intArray(size + 1);
+			var ja_ = GLPK.new_intArray(size + 1);
+			var ar_ = GLPK.new_doubleArray(size + 1);
+			for(int i = 0; i < size; ++i) {
+				GLPK.intArray_setitem(ia_, i + 1, ia[i] + 1);
+				GLPK.intArray_setitem(ja_, i + 1, ja[i] + 1);
+				GLPK.doubleArray_setitem(ar_, i + 1, ar[i]);
+			}
+			GLPK.glp_load_matrix(problem, size, ia_, ja_, ar_);
+			GLPK.delete_intArray(ia_);
+			GLPK.delete_intArray(ja_);
+			GLPK.delete_doubleArray(ar_);
+		}
+		public void LoadMatrix(int[] ia, int[] ja, double[] ar) {
+			LoadMatrix(ia.Count(), ia, ja, ar);
+		}
+		#endregion
+		#region 変数関係
+		// 変数を追加する
+		public void AddColumns(int n) {
+			GLPK.glp_add_cols(problem, n);
+		}
+		// 変数の数
+		public int ColumnsCount {
+			get { return GLPK.glp_get_num_cols(problem); }
+		}
+		// 変数の名前
+		public class columnName {
+			MipProblem mip;
+			public columnName(MipProblem mip) {
+				this.mip = mip;
+			}
+			public string this[int index] {
+				get { return GLPK.glp_get_col_name(mip.problem, index + 1); }
+				set { GLPK.glp_set_col_name(mip.problem, index + 1, value); }
+			}
+		}
+		public columnName ColumnName;
+		// 変数の範囲を設定する
+		public void SetColumnBounds(int index, BoundsType type, double lowerBound, double upperBound) {
+			GLPK.glp_set_col_bnds(problem, index + 1, (int)type, lowerBound, upperBound);
+		}
+		// 変数の種類
+		public class columnType {
+			MipProblem mip;
+			public columnType(MipProblem mip) {
+				this.mip = mip;
+			}
+			public BoundsType this[int index] {
+				get { return (BoundsType)GLPK.glp_get_col_type(mip.problem, index + 1); }
+			}
+		}
+		public columnType ColumnType;
+		// 変数の下限
+		public class columnLb {
+			MipProblem mip;
+			public columnLb(MipProblem mip) {
+				this.mip = mip;
+			}
+			public double this[int index] {
+				get { return GLPK.glp_get_col_lb(mip.problem, index + 1); }
+			}
+		}
+		public columnLb ColumnLB;
+		// 変数の上限
+		public class columnUb {
+			MipProblem mip;
+			public columnUb(MipProblem mip) {
+				this.mip = mip;
+			}
+			public double this[int index] {
+				get { return GLPK.glp_get_col_ub(mip.problem, index + 1); }
+			}
+		}
+		public columnUb ColumnUB;
+		// 変数の値
+		public class lpColumnValue {
+			MipProblem mip;
+			public lpColumnValue(MipProblem mip) {
+				this.mip = mip;
+			}
+			public double this[int index] {
+				get { return GLPK.glp_get_col_prim(mip.problem, index + 1); }
+			}
+		}
+		public lpColumnValue LpColumnValue;
+		#endregion
 	}
 }
--- GlpkWrapperCS/Program.cs
+++ GlpkWrapperCS/Program.cs
@@ -9,6 +9,7 @@ namespace GlpkSample {
 	using org.gnu.glpk;
 	class Program {
 		static void Main(string[] args) {
+			//Console.WriteLine($"{GLPK.GLP_FR} {GLPK.GLP_UP} {GLPK.GLP_LO} {GLPK.GLP_DB} {GLPK.GLP_FX}");
 			Func1();
 			Console.WriteLine("");
 			Func2();
@@ -42,7 +43,50 @@ namespace GlpkSample {
 				GLPK.glp_set_obj_coef(problem, 2, 6.0);
 				GLPK.glp_set_obj_coef(problem, 3, 4.0);
 				// 制約式の係数
-
+				var ia = GLPK.new_intArray(9 + 1);
+				var ja = GLPK.new_intArray(9 + 1);
+				var ar = GLPK.new_doubleArray(9 + 1);
+				GLPK.intArray_setitem(ia, 1, 1); //行番号を代入
+				GLPK.intArray_setitem(ia, 2, 1);
+				GLPK.intArray_setitem(ia, 3, 1);
+				GLPK.intArray_setitem(ia, 4, 2);
+				GLPK.intArray_setitem(ia, 5, 2);
+				GLPK.intArray_setitem(ia, 6, 2);
+				GLPK.intArray_setitem(ia, 7, 3);
+				GLPK.intArray_setitem(ia, 8, 3);
+				GLPK.intArray_setitem(ia, 9, 3);
+				GLPK.intArray_setitem(ja, 1, 1); //列番号を代入
+				GLPK.intArray_setitem(ja, 2, 2);
+				GLPK.intArray_setitem(ja, 3, 3);
+				GLPK.intArray_setitem(ja, 4, 1);
+				GLPK.intArray_setitem(ja, 5, 2);
+				GLPK.intArray_setitem(ja, 6, 3);
+				GLPK.intArray_setitem(ja, 7, 1);
+				GLPK.intArray_setitem(ja, 8, 2);
+				GLPK.intArray_setitem(ja, 9, 3);
+				GLPK.doubleArray_setitem(ar, 1, 1.0); //係数を代入
+				GLPK.doubleArray_setitem(ar, 2, 1.0);
+				GLPK.doubleArray_setitem(ar, 3, 1.0);
+				GLPK.doubleArray_setitem(ar, 4, 10.0);
+				GLPK.doubleArray_setitem(ar, 5, 4.0);
+				GLPK.doubleArray_setitem(ar, 6, 5.0);
+				GLPK.doubleArray_setitem(ar, 7, 2.0);
+				GLPK.doubleArray_setitem(ar, 8, 2.0);
+				GLPK.doubleArray_setitem(ar, 9, 6.0);
+				GLPK.glp_load_matrix(problem, 9, ia, ja, ar);
+				// 最適化
+				int result = GLPK.glp_simplex(problem, null);
+				// 結果表示
+				Console.WriteLine("");
+				Console.Write($"z = {GLPK.glp_get_obj_val(problem)}");
+				for(int i = 1; i <= GLPK.glp_get_num_cols(problem); ++i) {
+					Console.Write($" {GLPK.glp_get_col_name(problem, i)} = {GLPK.glp_get_col_prim(problem, i)}");
+				}
+				Console.WriteLine("");
+				// 後処理
+				GLPK.delete_intArray(ia);
+				GLPK.delete_intArray(ja);
+				GLPK.delete_doubleArray(ar);
 			}
 		}
 		// GlpkWrapperCSを使った場合
@@ -61,20 +105,32 @@ namespace GlpkSample {
 				problem.SetRowBounds(0, BoundsType.Upper, 0.0, 100.0);
 				problem.SetRowBounds(1, BoundsType.Upper, 0.0, 600.0);
 				problem.SetRowBounds(2, BoundsType.Upper, 0.0, 300.0);
-				/*// 変数の数・名前・範囲
-				GLPK.glp_add_cols(problem, 3);
-				GLPK.glp_set_col_name(problem, 1, "x1");
-				GLPK.glp_set_col_bnds(problem, 1, GLPK.GLP_LO, 0.0, 0.0);
-				GLPK.glp_set_col_name(problem, 2, "x2");
-				GLPK.glp_set_col_bnds(problem, 2, GLPK.GLP_LO, 0.0, 0.0);
-				GLPK.glp_set_col_name(problem, 3, "x3");
-				GLPK.glp_set_col_bnds(problem, 3, GLPK.GLP_LO, 0.0, 0.0);
+				// 変数の数・名前・範囲
+				problem.AddColumns(3);
+				problem.ColumnName[0] = "x1";
+				problem.ColumnName[1] = "x2";
+				problem.ColumnName[2] = "x3";
+				problem.SetColumnBounds(0, BoundsType.Lower, 0.0, 0.0);
+				problem.SetColumnBounds(1, BoundsType.Lower, 0.0, 0.0);
+				problem.SetColumnBounds(2, BoundsType.Lower, 0.0, 0.0);
 				// 目的関数の係数
-				GLPK.glp_set_obj_coef(problem, 1, 10.0);
-				GLPK.glp_set_obj_coef(problem, 2, 6.0);
-				GLPK.glp_set_obj_coef(problem, 3, 4.0);
-				// 制約式の係数*/
-
+				problem.ObjCoef[0] = 10.0;
+				problem.ObjCoef[1] = 6.0;
+				problem.ObjCoef[2] = 4.0;
+				// 制約式の係数
+				var ia = new int[] { 0, 0, 0, 1, 1, 1, 2, 2, 2 };
+				var ja = new int[] { 0, 1, 2, 0, 1, 2, 0, 1, 2 };
+				var ar = new double[] { 1.0, 1.0, 1.0, 10.0, 4.0, 5.0, 2.0, 2.0, 6.0 };
+				problem.LoadMatrix(ia, ja, ar);
+				// 最適化
+				problem.Simplex();
+				// 結果表示
+				Console.WriteLine("");
+				Console.Write($"z = {problem.LpObjVal}");
+				for(int i = 0; i < problem.ColumnsCount; ++i) {
+					Console.Write($" {problem.ColumnName[i]} = {problem.LpColumnValue[i]}");
+				}
+				Console.WriteLine("");
 			}
 		}
 	}
